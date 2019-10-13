8dbd7c0dbbad2b92b28ad5c9c97a0769ddb56777
tinatasha <tashambiti@gmail.com>
2019-10-13 12:53:56 +0300

    add models

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to="images")
+    image_name = models.CharField(max_length=300)
+    image_caption = models.TextField(max_length=500)
+    likes = models.IntegerField(default=0)
+    comments = models.TextField(max_length=700)
+    
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to="profile")
+    bio = models.TextField(max_length=800)
