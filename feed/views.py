db8da55b78ade2388c680cb13c17b75857f825b5
tinatasha <tashambiti@gmail.com>
2019-10-13 13:05:04 +0300

    modified changes

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import Image, Profile
 # Create your views here.
+def feed(request):
+    return render(request, "feed.html")
--- grammina/settings.py
+++ grammina/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'grammina.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'grammina',
+       'USER': 'moringa',
+   'PASSWORD':'benjaminthefaff',
+   }
 }
 
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
