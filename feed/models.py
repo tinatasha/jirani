9a88a8361e98ff203a5b8b7b3ca4e17b2aade457
tinatasha <tashambiti@gmail.com>
2019-10-13 18:01:10 +0300

    update models.py

--- feed/models.py
+++ feed/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to="images")
     image_name = models.CharField(max_length=300)
     image_caption = models.TextField(max_length=500)
     likes = models.IntegerField(default=0)
-    comments = models.TextField(max_length=700)
+    comments = models.TextField(blank=True, max_length=700)
+    time = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.image_name
     
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to="profile")
--- feed/views.py
+++ feed/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Image, Profile
 # Create your views here.
 def feed(request):
-    return render(request, "feed.html")
+    images = Image.objects.all()
+    return render(request, "feed.html", {"images":images})
