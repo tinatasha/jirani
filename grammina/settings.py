e28b1235ecd771c4d0fdb4a43ffcb7addc26de85
tinatasha <tashambiti@gmail.com>
2019-10-13 13:11:02 +0300

    modified changes

--- grammina/settings.py
+++ grammina/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'grammina.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
