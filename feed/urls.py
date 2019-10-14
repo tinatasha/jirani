a12498006e110f318fa3f210c64a692ce322f86c
tinatasha <tashambiti@gmail.com>
2019-10-14 05:30:06 +0300

    update views.py

--- feed/urls.py
+++ feed/urls.py
@@ -7,5 +7,3 @@ from . import views
 urlpatterns = [
     url(r'^', views.feed ),
 ]
-if settings.DEBUG:
-   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- feed/views.py
+++ feed/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Image, Profile
 # Create your views here.
 def feed(request):
-    images = Image.objects.all()
+    images = Image.objects.all().filter(time__lte=timezone.now()).order_by('-time')
     return render(request, "feed.html", {"images":images})
+
