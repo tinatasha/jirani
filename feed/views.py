602199909f78a83cfd3fadc4c4b6ddb3280baaa5
tinatasha <tashambiti@gmail.com>
2019-10-14 05:44:29 +0300

    modified changes

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Image, Profile
+from django.utils import timezone
 # Create your views here.
 def feed(request):
     images = Image.objects.all().filter(time__lte=timezone.now()).order_by('-time')
     return render(request, "feed.html", {"images":images})
 
+def image_details(request):
+    images = Image.objects.all().filter(time__lte=timezone.now())
+    
