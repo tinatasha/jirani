e2c58b3c551e774165161a0c0a9fc999f0733234
tinatasha <tashambiti@gmail.com>
2019-10-14 10:46:53 +0300

    update styles.css

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+function redHeart() {
+	var x = document.getElementById("likes");
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
