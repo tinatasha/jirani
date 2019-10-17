9277e4275fa07a40acd606684a7469e982c74b5b
tinatasha <tashambiti@gmail.com>
2019-10-17 05:22:25 +0300

    update changes

--- feed/models.py
+++ feed/models.py
@@ -25,7 +25,7 @@ class Image(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True, related_name="profile", on_delete=models.CASCADE)
     photo = models.ImageField(
-        upload_to="images/", max_length=255, null=True, blank=True, default=""
+        upload_to="profile/", max_length=255, null=True, blank=True, default=""
     )
     phone = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     bio = models.TextField()
